3dd4994180f5eead0bf59f435b326d7e34e3147e
Zach Panzarino <zachary@panzarino.com>
2018-01-18 02:19:52 -0500

    Work on hw2

--- hw/hw2/src/SinglyLinkedList.java
+++ hw/hw2/src/SinglyLinkedList.java
@@ -1,9 +1,9 @@
 /**
  * Your implementation of a circular singly linked list.
  *
- * @author YOUR NAME HERE
- * @userid YOUR USER ID HERE (e.g. gburdell3)
- * @GTID YOUR GT ID HERE (e.g. 900000000)
+ * @author Zachary Panzarino
+ * @userid zpanzarino3
+ * @GTID 903305160
  * @version 1.0
  */
 public class SinglyLinkedList<T> implements LinkedListInterface<T> {
@@ -13,57 +13,118 @@ public class SinglyLinkedList<T> implements LinkedListInterface<T> {
 
     @Override
     public void addAtIndex(T data, int index) {
-
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index provided.");
+        }
+        if (data == null) {
+            throw new IllegalArgumentException("Data can not be null.");
+        }
+        if (size == 0) {
+            head = new LinkedListNode<T>(data);
+            head.setNext(head);
+            size++;
+            return;
+        }
+        if (index == size) {
+            head.setNext(new LinkedListNode<T>(data, head.getNext()));
+            head = head.getNext();
+            size++;
+            return;
+        }
+        LinkedListNode<T> current = head;
+        for (int i = 0; i <= index; i++) {
+            current = current.getNext();
+        }
+        current.setNext(new LinkedListNode<T>(data, current.getNext()));
+        size++;
     }
 
     @Override
     public void addToFront(T data) {
-
+        addAtIndex(data, 0);
     }
 
     @Override
     public void addToBack(T data) {
-
+        addAtIndex(data, size);
     }
 
     @Override
     public T removeAtIndex(int index) {
-
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index provided.");
+        }
+        LinkedListNode<T> current = head;
+        for (int i = 0; i <= index; i++) {
+            current = current.getNext();
+        }
+        T value = current.getNext().getData();
+        current.setNext(current.getNext().getNext());
+        size--;
+        return value;
     }
 
     @Override
     public T removeFromFront() {
-
+        return removeAtIndex(0);
     }
 
     @Override
     public T removeFromBack() {
-
+        return removeAtIndex(size);
     }
 
     @Override
     public T removeLastOccurrence(T data) {
-
+        if (data == null) {
+            throw new IllegalArgumentException("Data can not be null.");
+        }
+        Integer last = null;
+        LinkedListNode<T> current = head.getNext();
+        for (int i = 0; i <= size; i++) {
+            if (current.getData().equals(data)) {
+                last = i;
+            }
+            current = current.getNext();
+        }
+        if (last == null) {
+            return null;
+        }
+        return removeAtIndex(last);
     }
 
     @Override
     public T get(int index) {
-
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index provided.");
+        }
+        LinkedListNode<T> current = head.getNext();
+        for (int i = 0; i <= index; i++) {
+            current = current.getNext();
+        }
+        return current.getData();
     }
 
     @Override
     public Object[] toArray() {
-
+        Object[] array = new Object[size];
+        LinkedListNode<T> current = head.getNext();
+        for (int i = 0; i < size; i++) {
+            array[i] = current;
+            current = current.getNext();
+        }
+        return array;
     }
 
     @Override
     public boolean isEmpty() {
-
+        return size == 0;
     }
 
     @Override
     public void clear() {
-
+        head = null;
+        size = 0;
     }
 
     @Override
