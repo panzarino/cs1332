8fec79aa583f1e7da5fff056feab7f6b8ffe66ea
Zach Panzarino <zachary@panzarino.com>
2018-02-05 23:29:39 -0500

    hw4 initial commit

---
+++ hw/hw4/src/BST.java
@@ -0,0 +1,101 @@
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Your implementation of a binary search tree.
+ *
+ * @author YOUR NAME HERE
+ * @userid YOUR USER ID HERE (i.e. gburdell3)
+ * @GTID YOUR GT ID HERE (i.e. 900000000)
+ * @version 1.0
+ */
+public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
+    // DO NOT ADD OR MODIFY INSTANCE VARIABLES.
+    private BSTNode<T> root;
+    private int size;
+
+    /**
+     * A no-argument constructor that should initialize an empty BST.
+     * DO NOT IMPLEMENT THIS CONSTRUCTOR!
+     */
+    public BST() {
+    }
+
+    /**
+     * Initializes the BST with the data in the Collection. The data
+     * should be added in the same order it is in the Collection.
+     *
+     * @param data the data to add to the tree
+     * @throws IllegalArgumentException if data or any element in data is null
+     */
+    public BST(Collection<T> data) {
+
+    }
+
+    @Override
+    public void add(T data) {
+
+    }
+
+    @Override
+    public T remove(T data) {
+
+    }
+
+    @Override
+    public T get(T data) {
+
+    }
+
+    @Override
+    public boolean contains(T data) {
+
+    }
+
+    @Override
+    public List<T> preorder() {
+
+    }
+
+    @Override
+    public List<T> postorder() {
+
+    }
+
+    @Override
+    public List<T> inorder() {
+
+    }
+
+    @Override
+    public List<T> levelorder() {
+
+    }
+
+    @Override
+    public int distanceBetween(T data1, T data2) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int height() {
+
+    }
+
+    @Override
+    public int size() {
+        // DO NOT MODIFY THIS METHOD
+        return size;
+    }
+
+    @Override
+    public BSTNode<T> getRoot() {
+        // DO NOT MODIFY THIS METHOD!
+        return root;
+    }
+}
