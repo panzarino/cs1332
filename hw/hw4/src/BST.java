a7b823abe64f6fe12d74548657f77805bf2ce8bc
Zach Panzarino <zachary@panzarino.com>
2018-02-16 21:26:13 -0500

    Add remove and distance methods to hw4

--- hw/hw4/src/BST.java
+++ hw/hw4/src/BST.java
@@ -33,7 +33,9 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
      */
     public BST(Collection<T> data) {
         if (data == null) {
-            throw new IllegalArgumentException("Can not add elements from a null collection.");
+            throw new IllegalArgumentException(
+                "Can not add elements from a null collection."
+            );
         }
         for (T item: data) {
             add(item);
@@ -43,7 +45,9 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
     @Override
     public void add(T data) {
         if (data == null) {
-            throw new IllegalArgumentException("A null value cannot be added to the tree.");
+            throw new IllegalArgumentException(
+                "A null value cannot be added to the tree."
+            );
         }
         if (size == 0) {
             root = new BSTNode<T>(data);
@@ -82,7 +86,88 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
 
     @Override
     public T remove(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Cannot search for null data.");
+        }
+        return removeHelper(data, root, null);
+    }
 
+    /**
+     * Recursively removes data from the tree
+     * @param data the data to remove from the tree
+     * @param node the current node in recursive calls
+     * @param parent the previous node in recursive calls
+     * @return the data removed from the tree
+     */
+    private T removeHelper(T data, BSTNode<T> node, BSTNode<T> parent) {
+        if (node == null) {
+            throw new NoSuchElementException(
+                "Element could not be found in tree."
+            );
+        }
+        int compared = data.compareTo(node.getData());
+        if (compared == 0) {
+            BSTNode<T> left = node.getLeft();
+            BSTNode<T> right = node.getRight();
+            T value = node.getData();
+            if (left != null && right != null) {
+                if (left.getRight() == null) {
+                    node.setData(left.getData());
+                    node.setLeft(null);
+                } else {
+                    node.setData(predecessor(left));
+                }
+            } else {
+                if (parent == null) {
+                    if (left == null && right == null) {
+                        root = null;
+                    } else if (left != null) {
+                        root = left;
+                    } else {
+                        root = right;
+                    }
+                }
+                if (data.compareTo(parent.getLeft().getData()) == 0) {
+                    if (left == null && right == null) {
+                        parent.setLeft(null);
+                    } else if (left != null) {
+                        parent.setLeft(left);
+                    } else {
+                        parent.setLeft(right);
+                    }
+                }
+                if (data.compareTo(parent.getRight().getData()) == 0) {
+                    if (left == null && right == null) {
+                        parent.setRight(null);
+                    } else if (left != null) {
+                        parent.setRight(left);
+                    } else {
+                        parent.setRight(right);
+                    }
+                }
+            }
+            size--;
+            return value;
+        }
+        if (compared > 0) {
+            return removeHelper(data, node.getRight(), node);
+        } else {
+            return removeHelper(data, node.getLeft(), node);
+        }
+    }
+
+    /**
+     * Recursively finds and removes the predecessor for the remove method
+     * @param node the node to search at
+     * @return the predecessor data
+     */
+    private T predecessor(BSTNode<T> node) {
+        BSTNode<T> right = node.getRight();
+        if (right.getRight() == null) {
+            node.setRight(null);
+            return right.getData();
+        }
+        return predecessor(right);
     }
 
     @Override
@@ -101,7 +186,9 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
      */
     private T getHelper(T data, BSTNode<T> node) {
         if (node == null) {
-            throw new NoSuchElementException("Element could not be found in tree.");
+            throw new NoSuchElementException(
+                "Element could not be found in tree."
+            );
         }
         if (node.getData().equals(data)) {
             return node.getData();
@@ -226,7 +313,58 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
 
     @Override
     public int distanceBetween(T data1, T data2) {
+        if (data1 == null || data2 == null) {
+            throw new IllegalArgumentException("Data cannot be null.");
+        }
+        BSTNode<T> common;
+        if (data1.compareTo(data2) > 0) {
+            common = commonAncestor(data1, data2, root);
+        } else {
+            common = commonAncestor(data2, data1, root);
+        }
+        return distance(data1, common, 0) + distance(data2, common, 0);
+    }
 
+    /**
+     * Recursively the deepest common ancestor of two values
+     * @param greater the greater of the values
+     * @param smaller the lesser of the values
+     * @param node the current node in recursive calls
+     * @return the common ancestor
+     */
+    private BSTNode<T> commonAncestor(T greater, T smaller, BSTNode<T> node) {
+        int comparedGreater = greater.compareTo(node.getData());
+        int comparedSmaller = smaller.compareTo(node.getData());
+        if (comparedGreater > 0 && comparedSmaller > 0) {
+            return commonAncestor(greater, smaller, node.getRight());
+        } else if (comparedGreater < 0 && comparedSmaller < 0) {
+            return commonAncestor(greater, smaller, node.getLeft());
+        } else {
+            return node;
+        }
+    }
+
+    /**
+     * Recursively calculate the distance between a root and a value
+     * @param value the value to search for
+     * @param node the current node in recursive calls
+     * @param sum the current sum to add to
+     * @return the distance between the root and the value
+     */
+    private int distance(T value, BSTNode<T> node, int sum) {
+        if (node == null) {
+            throw new NoSuchElementException(
+                "Could not find the value " + value + " in the tree."
+            );
+        }
+        int compared = value.compareTo(node.getData());
+        if (compared == 0) {
+            return sum;
+        } else if (compared > 0) {
+            return distance(value, node.getRight(), sum + 1);
+        } else {
+            return distance(value, node.getLeft(), sum + 1);
+        }
     }
 
     @Override
@@ -251,7 +389,9 @@ public class BST<T extends Comparable<? super T>> implements BSTInterface<T> {
         if (node.getRight() == null && node.getLeft() == null) {
             return 0;
         }
-        return Math.max(heightHelper(node.getLeft()), heightHelper(node.getRight()));
+        return Math.max(
+            heightHelper(node.getLeft()), heightHelper(node.getRight())
+        );
     }
 
     @Override
