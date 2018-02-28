b67c41ea65582d1fcdd210c96d669efc94fd26e6
Zach Panzarino <zachary@panzarino.com>
2018-02-27 20:00:52 -0500

    Create put and resize method hw6

--- hw/hw6/src/HashMap.java
+++ hw/hw6/src/HashMap.java
@@ -41,7 +41,37 @@ public class HashMap<K, V> implements HashMapInterface<K, V> {
 
     @Override
     public V put(K key, V value) {
-
+        if (key == null) {
+            throw new IllegalArgumentException("Key cannot be null.");
+        }
+        if (value == null) {
+            throw new IllegalArgumentException("Value cannot be null.");
+        }
+        if (size + 1 / ((double) table.length) > MAX_LOAD_FACTOR) {
+            resizeBackingTable(table.length * 2 + 1);
+        }
+        int index = key.hashCode() % table.length;
+        MapEntry<K, V> position = table[index];
+        if (position == null) {
+            table[index] = new MapEntry<K, V>(key, value);
+            size++;
+            return null;
+        }
+        for (MapEntry<K, V> i = position; i != null; i = i.getNext()) {
+            if (i.getKey().equals(key)) {
+                V temp = i.getValue();
+                i.setKey(key);
+                i.setValue(value);
+                size++;
+                return temp;
+            }
+            if (i.getNext() == null) {
+                i.setNext(new MapEntry<K, V>(key, value));
+                size++;
+                return null;
+            }
+        }
+        return null;
     }
 
     @Override
@@ -61,7 +91,8 @@ public class HashMap<K, V> implements HashMapInterface<K, V> {
 
     @Override
     public void clear() {
-
+        table = (MapEntry<K, V>[]) (new Object[INITIAL_CAPACITY]);
+        size = 0;
     }
 
     @Override
@@ -82,7 +113,27 @@ public class HashMap<K, V> implements HashMapInterface<K, V> {
 
     @Override
     public void resizeBackingTable(int length) {
-
+        if (length < table.length) {
+            throw new IllegalArgumentException(
+                "New length must be greater than current table length."
+            );
+        }
+        MapEntry<K, V>[] newTable = (MapEntry<K, V>[]) (new Object[length]);
+        for (MapEntry<K, V> entry : table) {
+            K key = entry.getKey();
+            V value = entry.getValue();
+            int index = key.hashCode() % newTable.length;
+            MapEntry<K, V> position = newTable[index];
+            if (position == null) {
+                newTable[index] = new MapEntry<K, V>(key, value);
+                size++;
+            }
+            while (position.getNext() != null) {
+                position = position.getNext();
+            }
+            position.setNext(new MapEntry<K, V>(key, value));
+        }
+        table = newTable;
     }
     
     @Override
