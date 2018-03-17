87d81cf8a633bccb853b75069f568e86a0d717d6
Zach Panzarino <zachary@panzarino.com>
2018-03-16 22:45:33 -0400

    Broken code avl

--- hw/hw7/src/AVL.java
+++ hw/hw7/src/AVL.java
@@ -42,46 +42,199 @@ public class AVL<T extends Comparable<? super T>> implements AVLInterface<T> {
         if (data == null) {
             throw new IllegalArgumentException("Data cannot be null.");
         }
+        AVLNode<T> newNode = new AVLNode<T>(data);
         if (root == null) {
-            root = new AVLNode<>(data);
+            root = newNode;
             size++;
+            setHeightBalance(root);
         } else {
-            addHelper(data, root);
+            addHelper(root, newNode);
         }
     }
 
     /**
-     * Recursively adds data in the correct location
-     * @param data the data to be added to the tree
+     * Recursively adds data in the correct location and balances tree
+     * @param previous the previous node in recursive calls
      * @param node the current node in recursive calls
      */
-    private void addHelper(T data, AVLNode<T> node) {
-        int compared = data.compareTo(node.getData());
+    private void addHelper(AVLNode<T> previous, AVLNode<T> node) {
+        int compared = previous.getData().compareTo(node.getData());
         if (compared == 0) {
             return;
         }
-        if (compared > 0) {
-            if (node.getRight() != null) {
-                addHelper(data, node.getRight());
+        if (compared < 0) {
+            if (previous.getRight() != null) {
+                addHelper(previous.getRight(), node);
             } else {
-                node.setRight(new AVLNode<T>(data));
+                previous.setRight(node);
                 size++;
-                // rebalance the tree
             }
         } else {
             if (node.getLeft() != null) {
-                addHelper(data, node.getLeft());
+                addHelper(previous.getLeft(), node);
             } else {
-                node.setLeft(new AVLNode<T>(data));
+                previous.setLeft(node);
                 size++;
-                // rebalance the tree
+            }
+        }
+        setHeightBalance(previous);
+        balance(previous);
+    }
+
+    /**
+     * Balances a node in the tree
+     * @param node node to be balanced
+     */
+    private void balance(AVLNode<T> node) {
+        int balance = node.getBalanceFactor();
+        if (balance > 1) {
+            if (node.getLeft() != null) {
+                if (node.getLeft().getBalanceFactor() >= 0) {
+                    rightRotate(node);
+                } else {
+                    leftRotate(node.getLeft());
+                    rightRotate(node);
+                }
+            }
+        } else if (balance < -1) {
+            if (node.getRight() != null) {
+                if (node.getRight().getBalanceFactor() <= 0) {
+                    leftRotate(node);
+                } else {
+                    rightRotate(node.getRight());
+                    leftRotate(node);
+                }
             }
         }
     }
 
+    /**
+     * Performs a right rotation
+     * @param node node to start rotation from
+     */
+    private void rightRotate(AVLNode<T> node) {
+        AVLNode<T> newNode = new AVLNode<T>(node.getData());
+        AVLNode<T> left = node.getLeft();
+        newNode.setRight(node.getRight());
+        newNode.setLeft(left.getRight());
+        node.setData(left.getData());
+        node.setRight(newNode);
+        node.setLeft(left.getLeft());
+        left.setLeft(null);
+        setHeightBalanceSub(node);
+    }
+
+    /**
+     * Performs a left rotation
+     * @param node node to start rotation from
+     */
+    private void leftRotate(AVLNode<T> node) {
+        AVLNode<T> newNode = new AVLNode<T>(node.getData());
+        AVLNode<T> right = node.getRight();
+        newNode.setLeft(node.getLeft());
+        newNode.setRight(right.getLeft());
+        node.setData(right.getData());
+        node.setLeft(newNode);
+        node.setRight(right.getRight());
+        right.setRight(null);
+        setHeightBalanceSub(node);
+    }
+
     @Override
     public T remove(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null.");
+        }
+        return removeHelper(data, root, null);
+    }
 
+    /**
+     * Recursively removes data from the tree
+     * @param data the data to remove from the tree
+     * @param node the current node in recursive calls
+     * @param parent the previous node in recursive calls
+     * @return the data removed from the tree
+     */
+    private T removeHelper(T data, AVLNode<T> node, AVLNode<T> parent) {
+        if (node == null) {
+            throw new NoSuchElementException(
+                "Element could not be found in tree."
+            );
+        }
+        int compared = data.compareTo(node.getData());
+        if (compared == 0) {
+            AVLNode<T> left = node.getLeft();
+            AVLNode<T> right = node.getRight();
+            T value = node.getData();
+            if (left != null && right != null) {
+                if (left.getRight() == null) {
+                    node.setData(left.getData());
+                    node.setLeft(null);
+                } else {
+                    node.setData(predecessor(left));
+                }
+            } else {
+                if (parent == null) {
+                    if (left == null && right == null) {
+                        root = null;
+                    } else if (left != null) {
+                        root = left;
+                    } else {
+                        root = right;
+                    }
+                } else {
+                    if (parent.getLeft() != null
+                            && data.compareTo(parent.getLeft().getData()) == 0) {
+                        if (left == null && right == null) {
+                            parent.setLeft(null);
+                        } else if (left != null) {
+                            parent.setLeft(left);
+                        } else {
+                            parent.setLeft(right);
+                        }
+                    }
+                    if (parent.getRight() != null
+                            && data.compareTo(parent.getRight().getData()) == 0) {
+                        if (left == null && right == null) {
+                            parent.setRight(null);
+                        } else if (left != null) {
+                            parent.setRight(left);
+                        } else {
+                            parent.setRight(right);
+                        }
+                    }
+                }
+            }
+            size--;
+            setHeightBalance(parent);
+            balance(parent);
+            return value;
+        }
+        if (compared > 0) {
+            T value = removeHelper(data, node.getRight(), node);
+            setHeightBalance(parent);
+            balance(parent);
+            return value;
+        } else {
+            T value = removeHelper(data, node.getLeft(), node);
+            setHeightBalance(parent);
+            balance(parent);
+            return value;
+        }
+    }
+
+    /**
+     * Recursively finds and removes the predecessor for the remove method
+     * @param node the node to search at
+     * @return the predecessor data
+     */
+    private T predecessor(AVLNode<T> node) {
+        AVLNode<T> right = node.getRight();
+        if (right.getRight() == null) {
+            node.setRight(right.getLeft());
+            return right.getData();
+        }
+        return predecessor(right);
     }
 
     @Override
@@ -152,7 +305,23 @@ public class AVL<T extends Comparable<? super T>> implements AVLInterface<T> {
 
     @Override
     public T getSecondLargest() {
-        
+        if (size < 2) {
+            throw new NoSuchElementException("Not enough elements in tree.");
+        }
+        return secondLargestHelper(root, root.getData());
+    }
+
+    /**
+     * Recursively finds the second largest element in the tree
+     * @param node the current node in recursive calls
+     * @param data the previous value in recursive calls
+     * @return the second largest element in the tree
+     */
+    private T secondLargestHelper(AVLNode<T> node, T data) {
+        if (node.getRight() != null) {
+            return secondLargestHelper(node.getRight(), node.getData());
+        }
+        return data;
     }
 
     @Override
@@ -193,24 +362,34 @@ public class AVL<T extends Comparable<? super T>> implements AVLInterface<T> {
 
     @Override
     public int height() {
-        return heightHelper(root);
+        return root.getHeight();
     }
 
     /**
-     * Recursively calculate the height of a node
-     * @param node the current node in recursive calls
-     * @return the height of the node
+     * Sets the height and balance factor for a node
+     * @param node the node to set data for
      */
-    private int heightHelper(AVLNode<T> node) {
-        if (node == null) {
-            return -1;
+    private void setHeightBalance(AVLNode<T> node) {
+        int leftHeight = (node.getLeft() != null) ?
+            node.getLeft().getHeight() : -1;
+        int rightHeight = (node.getRight() != null) ?
+            node.getRight().getHeight() : -1;
+        node.setHeight(Math.max(leftHeight, rightHeight) + 1);
+        node.setBalanceFactor(leftHeight - rightHeight);
+    }
+
+    /**
+     * Sets the height and balance factor for a subtree
+     * @param node the current node in recursive calls
+     */
+    private void setHeightBalanceSub(AVLNode<T> node) {
+        if (root.getLeft() != null) {
+            setHeightBalanceSub(node.getLeft());
         }
-        if (node.getRight() == null && node.getLeft() == null) {
-            return 0;
+        if (root.getRight() != null) {
+            setHeightBalanceSub(node.getRight());
         }
-        return Math.max(
-            heightHelper(node.getLeft()), heightHelper(node.getRight())
-        ) + 1;
+        setHeightBalance(node);
     }
 
     @Override
