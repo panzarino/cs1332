24631547d98187ea1819a349756085646349fa24
Zach Panzarino <zachary@panzarino.com>
2018-03-28 00:43:05 -0400

    Add bubble sort

---
+++ hw/hw8/src/Sorting.java
@@ -0,0 +1,225 @@
+import java.util.Comparator;
+import java.util.Random;
+
+/**
+ * Your implementation of various sorting algorithms.
+ *
+ * @author Zachary Panzarino
+ * @userid zpanzarino3
+ * @GTID 903305160
+ * @version 1.0
+ */
+public class Sorting {
+
+    /*
+
+    DELETE THIS FUNCTION
+
+     */
+
+    private static <T> void printArray(T[] arr) {
+        for (T x : arr) {
+            System.out.print(x);
+            System.out.print(" ");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    /**
+     * Implement bubble sort.
+     *
+     * It should be:
+     *  in-place
+     *  stable
+     *
+     * Have a worst case running time of:
+     *  O(n^2)
+     *
+     * And a best case running time of:
+     *  O(n)
+     *
+     * You may assume that the array doesn't contain any null elements.
+     *
+     * @throws IllegalArgumentException if the array or comparator is null
+     * @param <T> data type to sort
+     * @param arr the array that must be sorted after the method runs
+     * @param comparator the Comparator used to compare the data in arr
+     */
+    public static <T> void bubbleSort(T[] arr, Comparator<T> comparator) {
+        if (arr == null || comparator == null) {
+            throw new IllegalArgumentException("Arguments cannot be null.");
+        }
+        for (int x = 0; x < arr.length - 1; x++) {
+            boolean swap = false;
+            for (int i = 0; i < arr.length - x - 1; i++) {
+                if (comparator.compare(arr[i], arr[i + 1]) > 0) {
+                    swap(arr, i, i + 1);
+                    swap = true;
+                }
+            }
+            if (!swap) {
+                return;
+            }
+        }
+    }
+
+    /**
+     * Implement insertion sort.
+     *
+     * It should be:
+     *  in-place
+     *  stable
+     *
+     * Have a worst case running time of:
+     *  O(n^2)
+     *
+     * And a best case running time of:
+     *  O(n)
+     *
+     * You may assume that the array doesn't contain any null elements.
+     *
+     * @throws IllegalArgumentException if the array or comparator is null
+     * @param <T> data type to sort
+     * @param arr the array that must be sorted after the method runs
+     * @param comparator the Comparator used to compare the data in arr
+     */
+    public static <T> void insertionSort(T[] arr, Comparator<T> comparator) {
+
+    }
+
+    /**
+     * Implement selection sort.
+     *
+     * It should be:
+     *  in-place
+     *
+     * Have a worst case running time of:
+     *  O(n^2)
+     *
+     * And a best case running time of:
+     *  O(n^2)
+     *
+     *
+     * You may assume that the array doesn't contain any null elements.
+     *
+     * Note that there may be duplicates in the array.
+     *
+     * @throws IllegalArgumentException if the array or comparator is null
+     * @param <T> data type to sort
+     * @param arr the array that must be sorted after the method runs
+     * @param comparator the Comparator used to compare the data in arr
+     */
+    public static <T> void selectionSort(T[] arr, Comparator<T> comparator) {
+
+    }
+
+    /**
+     * Implement quick sort.
+     *
+     * Use the provided random object to select your pivots.
+     * For example if you need a pivot between a (inclusive)
+     * and b (exclusive) where b > a, use the following code:
+     *
+     * int pivotIndex = rand.nextInt(b - a) + a;
+     *
+     * It should be:
+     *  in-place
+     *
+     * Have a worst case running time of:
+     *  O(n^2)
+     *
+     * And a best case running time of:
+     *  O(n log n)
+     *
+     * You may assume that the array doesn't contain any null elements.
+     *
+     * Note that there may be duplicates in the array.
+     *
+     * Make sure you code the algorithm as you have been taught it in class.
+     * There are several versions of this algorithm and you may not get full
+     * credit if you do not use the one we have taught you!
+     *
+     * @throws IllegalArgumentException if the array or comparator or rand is
+     * null
+     * @param <T> data type to sort
+     * @param arr the array that must be sorted after the method runs
+     * @param comparator the Comparator used to compare the data in arr
+     * @param rand the Random object used to select pivots
+     */
+    public static <T> void quickSort(T[] arr, Comparator<T> comparator,
+                                     Random rand) {
+
+    }
+
+    /**
+     * Implement merge sort.
+     *
+     * It should be:
+     *  stable
+     *
+     * Have a worst case running time of:
+     *  O(n log n)
+     *
+     * And a best case running time of:
+     *  O(n log n)
+     *
+     * You may assume that the array doesn't contain any null elements.
+     *
+     * You can create more arrays to run mergesort, but at the end,
+     * everything should be merged back into the original T[]
+     * which was passed in.
+     *
+     * When splitting the array, if there is an odd number of elements, put the
+     * extra data on the right side.
+     *
+     * @throws IllegalArgumentException if the array or comparator is null
+     * @param <T> data type to sort
+     * @param arr the array to be sorted
+     * @param comparator the Comparator used to compare the data in arr
+     */
+    public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
+
+    }
+
+    /**
+     * Implement LSD (least significant digit) radix sort.
+     *
+     * Remember you CANNOT convert the ints to strings at any point in your
+     * code! Doing so may result in a 0 for the implementation.
+     *
+     * It should be:
+     *  stable
+     *
+     * Have a worst case running time of:
+     *  O(kn)
+     *
+     * And a best case running time of:
+     *  O(kn)
+     *
+     * Refer to the PDF for more information on LSD Radix Sort.
+     *
+     * You may use {@code java.util.ArrayList} or {@code java.util.LinkedList}
+     * if you wish, but it may only be used inside radix sort and any radix sort
+     * helpers. Do NOT use these classes with other sorts.
+     *
+     * @throws IllegalArgumentException if the array is null
+     * @param arr the array to be sorted
+     */
+    public static void lsdRadixSort(int[] arr) {
+
+    }
+
+    /**
+     * Swaps two elements in an array
+     * @param <T> data type to swap
+     * @param arr the array to perform the operation on
+     * @param pos1 index of the first element
+     * @param pos2 index of the second element
+     */
+    private static <T> void swap(T[] arr, int pos1, int pos2) {
+        T temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+    }
+}
