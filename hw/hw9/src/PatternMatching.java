da6b7837a6ab259384a79455dccbb96fe3325dce
Zach Panzarino <zachary@panzarino.com>
2018-04-05 02:44:41 -0400

    Add pattern matching lagorithms

--- hw/hw9/src/PatternMatching.java
+++ hw/hw9/src/PatternMatching.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -28,7 +29,9 @@ public class PatternMatching {
     public static List<Integer> bruteForce(CharSequence pattern,
         CharSequence text, CharacterComparator comparator) {
         if (pattern == null || pattern.length() == 0) {
-            throw new IllegalArgumentException("Pattern cannot be null or empty.");
+            throw new IllegalArgumentException(
+                "Pattern cannot be null or empty."
+            );
         }
         if (text == null) {
             throw new IllegalArgumentException("Text cannot be null.");
@@ -36,8 +39,8 @@ public class PatternMatching {
         if (comparator == null) {
             throw new IllegalArgumentException("Comparator cannot be null.");
         }
-        List<Integer> output = new LinkedList<Integer>();
-        for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
+        List<Integer> matches = new LinkedList<Integer>();
+        for (int i = 0; i <= text.length() - pattern.length(); i++) {
             boolean match = true;
             for (int x = 0; x < pattern.length() && match; x++) {
                 if (comparator.compare(
@@ -47,10 +50,10 @@ public class PatternMatching {
                 }
             }
             if (match) {
-                output.add(i);
+                matches.add(i);
             }
         }
-        return output;
+        return matches;
     }
 
     /**
@@ -68,7 +71,41 @@ public class PatternMatching {
      */
     public static List<Integer> kmp(CharSequence pattern, CharSequence text,
                                     CharacterComparator comparator) {
-        return null;
+        if (pattern == null || pattern.length() == 0) {
+            throw new IllegalArgumentException(
+                    "Pattern cannot be null or empty."
+            );
+        }
+        if (text == null) {
+            throw new IllegalArgumentException("Text cannot be null.");
+        }
+        if (comparator == null) {
+            throw new IllegalArgumentException("Comparator cannot be null.");
+        }
+        int[] table = buildFailureTable(pattern, comparator);
+        List<Integer> matches = new LinkedList<Integer>();
+        int i = 0;
+        int j = 0;
+        while (i <= text.length() - pattern.length()) {
+            while (j < pattern.length()
+                && comparator.compare(
+                    pattern.charAt(j),
+                    text.charAt(i + j)
+                ) == 0) {
+                j++;
+            }
+            if (j == 0) {
+                i++;
+            } else {
+                if (j == pattern.length()) {
+                    matches.add(i);
+                }
+                int nextAlignment = table[j - 1];
+                i = i + j - nextAlignment;
+                j = nextAlignment;
+            }
+        }
+        return matches;
     }
 
     /**
@@ -99,7 +136,32 @@ public class PatternMatching {
      */
     public static int[] buildFailureTable(CharSequence pattern,
                                           CharacterComparator comparator) {
-        return null;
+        if (pattern == null) {
+            throw new IllegalArgumentException(
+                "Pattern cannot be null or empty."
+            );
+        }
+        if (comparator == null) {
+            throw new IllegalArgumentException("Comparator cannot be null.");
+        }
+        int[] table = new int[pattern.length()];
+        if (pattern.length() == 0) {
+            return table;
+        }
+        int i = 0;
+        int j = 1;
+        while (j < pattern.length()) {
+            if (comparator.compare(
+                pattern.charAt(i),
+                pattern.charAt(j)) == 0) {
+                table[j++] = ++i;
+            } else if (i == 0) {
+                table[j++] = 0;
+            } else {
+                i = table[i - 1];
+            }
+        }
+        return table;
     }
 
     /**
@@ -118,7 +180,46 @@ public class PatternMatching {
      */
     public static List<Integer> boyerMoore(CharSequence pattern,
                        CharSequence text, CharacterComparator comparator) {
-        return null;
+        if (pattern == null || pattern.length() == 0) {
+            throw new IllegalArgumentException(
+                    "Pattern cannot be null or empty."
+            );
+        }
+        if (text == null) {
+            throw new IllegalArgumentException("Text cannot be null.");
+        }
+        if (comparator == null) {
+            throw new IllegalArgumentException("Comparator cannot be null.");
+        }
+        Map<Character, Integer> map = buildLastTable(pattern);
+        List<Integer> matches = new LinkedList<Integer>();
+        int i = 0;
+        while (i <= text.length() - pattern.length()) {
+            int j = pattern.length() - 1;
+            while (j >= 0
+                && comparator.compare(
+                    text.charAt(i + j),
+                    pattern.charAt(j)
+                ) == 0) {
+                j--;
+            }
+            if (j == -1) {
+                matches.add(i++);
+            } else {
+                int shiftedIndex;
+                if (map.containsKey(text.charAt(i + j))) {
+                    shiftedIndex = map.get(text.charAt(i + j));
+                } else {
+                    shiftedIndex = -1;
+                }
+                if (shiftedIndex < j) {
+                    i += j - shiftedIndex;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return matches;
     }
 
     /**
@@ -148,7 +249,14 @@ public class PatternMatching {
      *         to their last occurrence in the pattern
      */
     public static Map<Character, Integer> buildLastTable(CharSequence pattern) {
-        return null;
+        if (pattern == null) {
+            throw new IllegalArgumentException("Pattern cannot be null.");
+        }
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < pattern.length(); i++) {
+            map.put(pattern.charAt(i), i);
+        }
+        return map;
     }
 
 }
